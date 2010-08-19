983901717fd2acce2ce588ea9d8713e78527a019
Intel <chemicstry@gmail.com>
2010-08-19 23:51:25 +0300

    Revert "[521] Removed AH Bot. You can find it on branch 'with_ahbot'."
    This reverts commit a3bd415516404b4feb319b61f45b127dddb66070.

--- src/server/game/World/World.cpp
+++ src/server/game/World/World.cpp
@@ -71,6 +71,7 @@
 #include "CharacterDatabaseCleaner.h"
 #include "ScriptMgr.h"
 #include "WeatherMgr.h"
+#include "AuctionHouseBot.h"
 
 volatile bool World::m_stopEvent = false;
 uint8 World::m_ExitCode = SHUTDOWN_EXIT_CODE;
@@ -1740,6 +1741,9 @@ void World::SetInitialWorldSettings()
     sLog.outString("Starting objects Pooling system...");
     sPoolMgr.Initialize();
 
+    sLog.outString("Initialize AuctionHouseBot...");
+    auctionbot.Initialize();
+
     // possibly enable db logging; avoid massive startup spam by doing it here.
     if (sLog.GetLogDBLater())
     {
@@ -1906,6 +1910,7 @@ void World::Update(uint32 diff)
     /// <ul><li> Handle auctions when the timer has passed
     if (m_timers[WUPDATE_AUCTIONS].Passed())
     {
+        auctionbot.Update();
         m_timers[WUPDATE_AUCTIONS].Reset();
 
         ///- Update mails (return old mails with item, or delete them)
