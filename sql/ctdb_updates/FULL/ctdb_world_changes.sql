3ec187a82018c3da0e09b3a0bac74b016c127d4a
Intel <chemicstry@gmail.com>
2010-08-20 23:57:42 +0300

    [527] SQL Typo Fix. Thanks again to Jonok.

--- sql/ctdb_updates/FULL/ctdb_world_changes.sql
+++ sql/ctdb_updates/FULL/ctdb_world_changes.sql
@@ -4,4 +4,4 @@ DELETE FROM `npc_trainer` WHERE `entry`='925' AND `spell`='20217'
 
 -- 2_world_blood_elf_hunter_quest.sql
 -- This fixes bug that you could use Seek the Farstriders as quest chain start quest.
-UPDATE `quest_temlpate` SET `PrevQuestId` = '0' WHERE `entry` = '9484'; 
+UPDATE `quest_template` SET `PrevQuestId` = '0' WHERE `entry` = '9484'; 
